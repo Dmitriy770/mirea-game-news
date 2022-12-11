885d3b816a42a28d3daa6e1b02814844b47f6d5b
stolevarov <zdima08@gmail.com>
2022-12-11 16:07:41 +0300

    update base URL

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  base: '/mirea-game-news/',
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/mirea-game-news/'
+      : '/',
   transpileDependencies: true,
   pages:{
     index:{
